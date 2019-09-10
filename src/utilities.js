4c3fe283467983626dc8914b5c5a4df286fa2d4a
cc-nati <nati.levi@climacell.co>
2019-09-10 21:52:57 +0300

    feat(): group by util

---
+++ src/utilities.js
@@ -0,0 +1,22 @@
+const groupBy = ({ arr, criteria }) => {
+    return arr.reduce(function (obj, item) {
+
+        // Check if the criteria is a function to run on the item or a property of it
+        const key = typeof criteria === 'function' ? criteria(item) : item[criteria];
+
+        // If the key doesn't exist yet, create it
+        if (!obj.hasOwnProperty(key)) {
+            obj[key] = [];
+        }
+
+        // Push the value to the object
+        obj[key].push(item);
+
+        // Return the object to the next item in the loop
+        return obj;
+
+    }, {});
+};
+
+
+export { groupBy };
